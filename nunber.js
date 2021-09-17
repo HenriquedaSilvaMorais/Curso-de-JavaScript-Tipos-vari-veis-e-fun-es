8d08bd9ea6dd6b4e83d0681d9e3d3abfe87afd5c
Henrique da Silva Morais <henriquedasilvamorais275@gmail.com>
2021-09-17 18:08:00 -0300

    operadores e funcoes

---
+++ nunber.js
@@ -0,0 +1,11 @@
+const numero1 = 12;
+const numero2 = 13;
+const numerof = .5;
+const numerof2 = 2.2;
+
+const soma = numero1 + numero2;
+const soma2 = numerof + numerof2;
+const mult = numero1 + numero2;
+console.log(soma)
+console.log(soma2)
+console.log(mult)
---
+++ treino.js
@@ -0,0 +1,19 @@
+const nome = "henrique";
+let idade = 19;
+let curso = "CC";
+
+
+function mudarCurso(novoCurso){
+    novoCurso = curso;
+}
+
+
+function faserAniversario(){
+    let idade = idade+1;
+}
+
+
+console.log(novoCurso("CI"));
+console.log(String(nome));
+console.log(String(idade));
+console.log(String(curso));
